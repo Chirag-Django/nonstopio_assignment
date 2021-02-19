b11e0a6dd82d7e2314ccf22cf019b97c0902a52f
CKdave <chirag.django@gmail.com>
2021-02-19 19:02:49 +0530

    added search functionality

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list,product_details,index,search_products
 
 urlpatterns = [
-    path('product_list/',product_list,name='product_list')
+    path('home/',index,name='home'),
+    path('product_list/',product_list,name='product_list'),
+    path('product_details/<int:id>/',product_details,name='product_details'),
+    path('search/',search_products,name='search_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import Product
 from django.http import Http404
+from django.db.models import Q
 
 # Create your views here.
 
+def index(request):
+    return render(request,'index.html')
 
 def product_list(request):
     products = Product.objects.all()
     return render(request,'product_list.html',{'all_products':products})
 
-def productDetails(request,id):
+def product_details(request,id):
     global message
     try:
         product_details = Product.objects.get(pk=id)
@@ -19,3 +22,12 @@ def productDetails(request,id):
     except Product.DoesNotExist:
         raise Http404("Product Doesnot Exists")
     return render(request,'product_details.html',{'product_details': product_details,'message': message})
+
+def search_products(request):
+    search_product = request.GET.get('q')
+    if search_product:
+        products = Product.objects.filter(Q(product_shape__icontains=search_product)|Q(product_location__icontains=search_product))
+    else:
+        products = Product.objects.none()
+    return render(request,'product_list.html',{'all_products':products,'search_product':search_product})
+
